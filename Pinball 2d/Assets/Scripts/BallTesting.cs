2d35f8b77e3c1b036ec57aadfde93f33e60443bc
Shadovar <cerez@ucsc.edu>
2019-10-21 04:53:10 -0700

    Added Initial Unity Work
    Rotating Flipper, Ball that rolls downward.

---
+++ Pinball 2d/Assets/Scripts/BallTesting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTesting : MonoBehaviour
+{
+
+    public Rigidbody2D rigidBall;
+    public Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rigidBall.AddForce(transform.up * 3, ForceMode2D.Force);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        transform.position = startPos;
+    }
+}
---
+++ Pinball 2d/Assets/Scripts/FlipperController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.rotation.z > 0)
+        {
+            transform.Rotate(new Vector3(0, 0, -5));
+        }
+        else if(transform.rotation.z < 0)
+        {
+            transform.Rotate(new Vector3(0, 0, 5));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.rotation.z < 100)
+        {
+            transform.Rotate(new Vector3(0, 0, 10));
+        }
+    }
+}
