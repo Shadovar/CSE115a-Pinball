a13bb9b578f7eb75c85c090bdd3cc139355f8e51
Jordan Christian Leggett <jleggett@ucsc.edu>
2019-10-31 15:43:06 -0700

    Made garbage launcher

---
+++ Pinball 2d/Assets/Scripts/LauncherSpring.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherSpring : MonoBehaviour
+{
+    public static KeyCode launchCode = KeyCode.Space;
+    public static Vector3 launchPos = new Vector3(8.075f, -0.5f, 0);
+    public BallTesting newBall;
+    private int numFramesHeld = 0;
+    int MaxNumFramesHold = 30 * 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newBall.startPos = launchPos;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(launchCode))
+        {
+            numFramesHeld = 0;
+        }
+        else if (Input.GetKey(launchCode))
+        {
+            numFramesHeld = (numFramesHeld < MaxNumFramesHold) ? (numFramesHeld + 1) : MaxNumFramesHold;
+            newBall.rigidBall.position = new Vector3(launchPos.x, launchPos.y - (1.0f + (numFramesHeld / (float)MaxNumFramesHold)));
+        }
+        else if (Input.GetKeyUp(launchCode))
+        {
+            float applyForce = (numFramesHeld * 300) / (float)MaxNumFramesHold;
+            newBall.rigidBall.position = launchPos;
+            newBall.rigidBall.AddForce(new Vector2(0.0f, applyForce), ForceMode2D.Impulse);
+            Debug.Log("Launch Intensity: " + applyForce);
+            numFramesHeld = 0;
+        }
+        transform.localScale = new Vector3(1, 1.0f - (numFramesHeld / (float)MaxNumFramesHold), 0);
+        transform.localPosition = new Vector3(launchPos.x, launchPos.y - (2.0f + (numFramesHeld / (float)MaxNumFramesHold)));
+    }
+}
