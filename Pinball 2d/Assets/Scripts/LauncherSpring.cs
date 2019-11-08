67efc831b125530ef3a91fc3901e0950a46fa409
Jordan Christian Leggett <jleggett@ucsc.edu>
2019-11-07 16:59:45 -0800

    Commiting revert
    Cuz itsa mess

--- Pinball 2d/Assets/Scripts/LauncherSpring.cs
+++ Pinball 2d/Assets/Scripts/LauncherSpring.cs
@@ -7,7 +7,7 @@ public class LauncherSpring : MonoBehaviour
     public LaunchBarrier barrier;
     public KeyCode launchCode = KeyCode.Space;
     public Vector3 launchPos; //= new Vector3(8.075f, -0.5f, 0);
-    public pinballScript newBall, cb;
+    public pinballScript newBall;
     public float springHeight;
     private int numFramesHeld = 0;
     int MaxNumFramesHold = 30 * 3;
@@ -27,21 +27,17 @@ public class LauncherSpring : MonoBehaviour
         if (Input.GetKeyDown(launchCode))
         {
             numFramesHeld = 0;
-            if (cb != null)
-                DestroyImmediate(cb.gameObject);
-            cb = Instantiate(newBall, new Vector3(launchPos.x, launchPos.y - (numFramesHeld / (float)MaxNumFramesHold) * springHeight), Quaternion.identity);
-
         }
         else if (Input.GetKey(launchCode))
         {
             numFramesHeld = (numFramesHeld < MaxNumFramesHold) ? (numFramesHeld + 1) : MaxNumFramesHold;
-            cb.rigidBall.position = new Vector3(launchPos.x, launchPos.y - (numFramesHeld / (float)MaxNumFramesHold) * springHeight);
+            newBall.rigidBall.position = new Vector3(launchPos.x, launchPos.y - (numFramesHeld / (float)MaxNumFramesHold) * springHeight);
         }
         else if (Input.GetKeyUp(launchCode))
         {
             float applyForce = 150 + (numFramesHeld * 300) / (float)MaxNumFramesHold;
-            cb.rigidBall.position = new Vector3(launchPos.x, launchPos.y - (numFramesHeld / (float)MaxNumFramesHold) * springHeight);
-            cb.rigidBall.AddForce(new Vector2(0.0f, applyForce), ForceMode2D.Impulse);
+            newBall.rigidBall.position = new Vector3(launchPos.x, launchPos.y - (numFramesHeld / (float)MaxNumFramesHold) * springHeight);
+            newBall.rigidBall.AddForce(new Vector2(0.0f, applyForce), ForceMode2D.Impulse);
             Debug.Log("Launch Intensity: " + applyForce);
             numFramesHeld = 0;
             barrier.collider.isTrigger = true;
