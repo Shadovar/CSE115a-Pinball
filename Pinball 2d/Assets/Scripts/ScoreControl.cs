6d6c3d544890701703e4fee937303b8086c56171
Jordan Christian Leggett <jleggett@ucsc.edu>
2019-11-13 15:21:25 -0800

    Added score saving
    Still need to work out a high-score system.
    Also added gitignore to VSPROJ directory, its still being very rude though. Need to work on that too.

--- Pinball 2d/Assets/Scripts/ScoreControl.cs
+++ Pinball 2d/Assets/Scripts/ScoreControl.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreControl : MonoBehaviour
 {
     public static int scorevalue = 0;
+    public const string defaultScorePath = "score.txt";
     Text score;
     // Start is called before the first frame update
     void Start()
@@ -17,5 +19,38 @@ public class ScoreControl : MonoBehaviour
     void Update()
     {
         score.text = "score: " + scorevalue;
+    }
+
+    public static void Load(string path = defaultScorePath)
+    {
+        try
+        {
+            string score = File.ReadAllText(path);
+            if (!int.TryParse(score, out scorevalue))
+            {
+                Debug.Log("Failed to parse score file. Defaulting to 0.");
+                scorevalue = 0;
+            }
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("Failed to load score. Defaulting to 0.");
+            scorevalue = 0;
+            throw;
+        }
+    }
+
+    public static void Save(string path = defaultScorePath)
+    {
+        try
+        {
+            string score = scorevalue.ToString();
+            File.WriteAllText(path, score);
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("Failed to save score file.");
+            throw;
+        }
     }
 }
