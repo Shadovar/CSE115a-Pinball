92ffaa03810dcc05978ce1be698ea8d6fb25090f
zhyang98 <49853737+zhyang98@users.noreply.github.com>
2019-11-17 23:45:59 -0800

    Bonus points added

--- Pinball 2d/Assets/Scripts/pinballScript.cs
+++ Pinball 2d/Assets/Scripts/pinballScript.cs
@@ -9,6 +9,7 @@ public class pinballScript : MonoBehaviour
     public Vector3 startPos;
     public AudioClip hitWallSound;
     public AudioSource hitWallSource;
+    public int bonustracker = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,14 +36,56 @@ public class pinballScript : MonoBehaviour
         }
         if (collision.transform.name == "Flipper")
         {
-            ScoreControl.scorevalue += 10;
             Debug.Log("Hit Flipper");
         }
+        // hit bumpers
+        if (collision.transform.name == "Triangle")
+        {
+            ScoreControl.scorevalue += 10;
+        }
+        // lose the game
         if (collision.transform.name == "YouLose")
         {
             ScoreControl.scorevalue = 0;
             transform.position = startPos;
         }
-
+        // bonus points
+        if (collision.transform.name == "Bonus1")
+        {
+            bonustracker += 1;
+            int bonus1 = 0;
+            bonus1++;
+            Debug.Log(bonus1);
+            if (bonustracker >= 3 && bonus1 == 1)
+            {
+                ScoreControl.scorevalue += 100;
+                bonustracker = 0;
+                bonus1 = 0;
+            }
+        }
+        if (collision.transform.name == "Bonus2")
+        {
+            bonustracker += 1;
+            int bonus2 = 0;
+            bonus2++;
+            if (bonustracker >= 3 && bonus2 == 1)
+            {
+                ScoreControl.scorevalue += 100;
+                bonustracker = 0;
+                bonus2 = 0;
+            }
+        }
+        if (collision.transform.name == "Bonus3")
+        {
+            bonustracker += 1;
+            int bonus3 = 0;
+            bonus3++;
+            if (bonustracker >= 3 && bonus3 == 1)
+            {
+                ScoreControl.scorevalue += 100;
+                bonustracker = 0;
+                bonus3 = 0;
+            }
+        }
     }
 }
